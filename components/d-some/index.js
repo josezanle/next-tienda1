8e12281442395db0a3e50ae8dbeca1f4ffbb0515
josezanle <jose.rios.lm@gmail.com>
2021-04-14 12:01:09 -0300

    some changes

--- components/d-some/index.js
+++ components/d-some/index.js
@@ -1,18 +1,31 @@
 const Some = () => {
   return (
     <div className="some">
+      <h2>
+        Somos Lideres <br /> en el Mercado
+      </h2>
+
       <style jsx>{`
         .some {
           width: 100vw;
-          height: 70vh;
-          background-attachment: fixed;
-          background-image: url(/images/paying.jpg);
-          background-size: cover;
-          background-repeat: no-repeat;
-          background-position: center;
           display: flex;
           justify-content: center;
           align-items: center;
+        }
+
+        h2 {
+          background-attachment: fixed;
+          background-image: url(/images/paying.jpg);
+          background-clip: text;
+          -webkit-background-clip: text;
+          color: transparent;
+          font-size: 12em;
+          line-height: 0.8em;
+          height: 100%;
+          width: 100vw;
+          text-align: center;
+          letter-spacing: -9px;
+          overflow: hidden;
         }
       `}</style>
     </div>
