58929de0098f51b3ad421c41cf22688acff0d0ca
josezanle <jose.rios.lm@gmail.com>
2021-04-16 14:03:18 -0300

    add de art

--- utils/targets.js
+++ utils/targets.js
@@ -1,14 +1,98 @@
 export const Targets = [
   {
-    photo: "/images/pack.jpg",
+    photo: "/images/articulos/art1.jpg",
     description: "Descripcion de oferta",
   },
   {
-    photo: "/images/cunning.jpg",
+    photo: "/images/articulos/art2.jpg",
     description: "Descripcion de oferta",
   },
   {
-    photo: "/images/pack2.png",
+    photo: "/images/articulos/art3.jpg",
+    description: "Descripcion de oferta",
+  },
+  {
+    photo: "/images/articulos/art4.jpg",
+    description: "Descripcion de oferta",
+  },
+  {
+    photo: "/images/articulos/art5.jpg",
+    description: "Descripcion de oferta",
+  },
+  {
+    photo: "/images/articulos/art6.jpg",
+    description: "Descripcion de oferta",
+  },
+  {
+    photo: "/images/articulos/art7.jpg",
+    description: "Descripcion de oferta",
+  },
+  {
+    photo: "/images/articulos/art8.jpg",
+    description: "Descripcion de oferta",
+  },
+  {
+    photo: "/images/articulos/art9.jpg",
+    description: "Descripcion de oferta",
+  },
+  {
+    photo: "/images/articulos/art10.jpg",
+    description: "Descripcion de oferta",
+  },
+  {
+    photo: "/images/articulos/art11.jpg",
+    description: "Descripcion de oferta",
+  },
+  {
+    photo: "/images/articulos/art12.jpg",
+    description: "Descripcion de oferta",
+  },
+  {
+    photo: "/images/articulos/art13.jpg",
+    description: "Descripcion de oferta",
+  },
+  {
+    photo: "/images/articulos/art14.jpg",
+    description: "Descripcion de oferta",
+  },
+  {
+    photo: "/images/articulos/art15.jpg",
+    description: "Descripcion de oferta",
+  },
+  {
+    photo: "/images/articulos/art16.jpg",
+    description: "Descripcion de oferta",
+  },
+  {
+    photo: "/images/articulos/art17.jpg",
+    description: "Descripcion de oferta",
+  },
+  {
+    photo: "/images/articulos/art18.jpg",
+    description: "Descripcion de oferta",
+  },
+  {
+    photo: "/images/articulos/art19.jpg",
+    description: "Descripcion de oferta",
+  },
+  {
+    photo: "/images/articulos/art20.jpg",
+    description: "Descripcion de oferta",
+  },
+  {
+    photo: "/images/articulos/art21.jpg",
+    description: "Descripcion de oferta",
+  },
+  {
+    photo: "/images/articulos/art22.jpg",
+    description: "Descripcion de oferta",
+  },
+  {
+    photo: "/images/articulos/art23.jpg",
+    description: "Descripcion de oferta",
+  },
+  {
+    photo: "/images/articulos/art24.jpg",
     description: "Descripcion de oferta",
   },
 ];
